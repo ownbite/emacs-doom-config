96871c78f4a38508896cd945b675dac9c54e3c28
Linus Wallin <linus.wallin@husqvarnagroup.com>
2020-07-19 11:59:56 +0200

    Cleanup config file

--- config.el
+++ config.el
@@ -1,9 +1,9 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here
 (server-start)
 
-;; Make it possible to quickly swap over to iterm
+;;;;;;;;;;;;;;;;;;;;;;
+;; CUSTOM FUNCTIONS ;;
+;;;;;;;;;;;;;;;;;;;;;;
 (defun open-iterm ()
   (interactive)
 
@@ -19,38 +19,30 @@
   (evil-window-down 1)
   (multi-term))
 
-(defun pipenv-activate-and-reload ()
-  (interactive)
-  (pipenv-deactivate)
-  (pipenv-activate))
 
-(setq flycheck-check-syntax-automatically '(mode-enabled save))
-
-;; Custom keybindings
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; Custom keybindings ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
 (map!
  :n "M-m" 'evil-mc-make-and-goto-next-match
 
  (:leader
-    ;; :desc "jedi code completion"   :n  "m"   #'jedi:complete
     :desc "M-x"                    :nv ":"   #'execute-extended-command
 
     :desc "Delete the window"      :n  "q"   #'delete-window
 
-    ;; :desc "Horizonal Split"        :n  "s"   #'split-window-below
-    ;; :desc "Vertical Split"         :n  "v"   #'split-window-right
-
-
     (:desc "toggle" :prefix "t"
       :desc "Toggle terminal"      :n  "t"   #'open-terminal-in-new-window)
-
-    ;; (:desc "git" :prefix "g"
-    ;;   :desc "Magit status"         :n  "g"   #'magit-status)
 
     (:desc "code" :prefix "c"
       :desc "Toggle comments"      :n  "c"   #'comment-line
       :desc "Run pytest repeat"    :n  "t"   #'python-pytest-repeat
-      :desc "Run pytest config"    :n  "y"   #'python-pytest-popup
-      :desc "Activate pipenv"      :n  "a"   #'pipenv-activate-and-reload)))
+      :desc "Run pytest config"    :n  "y"   #'python-pytest-popup)))
+
+
+;;;;;;;;;;;;;;
+;; SETTINGS ;;
+;;;;;;;;;;;;;;
 
 ;; Autocompletion
 (setq company-idle-delay 0.2
@@ -64,3 +56,5 @@
 
 ;; Do not guess auto root
 (setq lsp-auto-guess-root nil)
+
+(setq flycheck-check-syntax-automatically '(mode-enabled save))

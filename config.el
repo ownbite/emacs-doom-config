889f20a9f0570c1c4f944eb6e533697c351d9945
Linus Wallin <linus.wallin@husqvarnagroup.com>
2023-09-01 09:44:45 +0200

    Add config for javascript development

--- config.el
+++ config.el
@@ -92,3 +92,8 @@
    (getenv "PATH")
   )
 )
+
+;; Javascript development blarg
+(add-hook 'js2-mode-hook 'skewer-mode)
+(add-hook 'css-mode-hook 'skewer-css-mode)
+(add-hook 'html-mode-hook 'skewer-html-mode)
